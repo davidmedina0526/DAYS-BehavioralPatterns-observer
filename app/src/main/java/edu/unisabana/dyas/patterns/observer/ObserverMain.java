c691a1abc814058754dde43127e88ef4c361a3dc
davidmedina0526 <davidmebu@unisabana.edu.co>
2025-02-16 22:32:38 -0500

    Entrega 1

--- app/src/main/java/edu/unisabana/dyas/patterns/observer/ObserverMain.java
+++ app/src/main/java/edu/unisabana/dyas/patterns/observer/ObserverMain.java
@@ -2,46 +2,52 @@ package edu.unisabana.dyas.patterns.observer;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
-import java.util.Observable;
+
 import edu.unisabana.dyas.patterns.observer.impl.ConfigurationManager;
 import edu.unisabana.dyas.patterns.observer.impl.observers.DateFormatObserver;
 import edu.unisabana.dyas.patterns.observer.impl.observers.MoneyFormatObserver;
 
 /**
- * @author cesarvefe
- 
+ * @author 
  */
 public class ObserverMain {
 
     public static void main(String[] args) {
+        // Obtenemos la instancia única de ConfigurationManager
         ConfigurationManager conf = ConfigurationManager.getInstance();
-        
-        //Se establecen los valores por default.
+
+        // Creamos los observadores que están interesados en los cambios de la configuración
+        DateFormatObserver dateFormatObserver = new DateFormatObserver();
+        MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
+
+        // Registramos (suscribimos) los observadores en el observable
+        conf.addObserver(dateFormatObserver);
+        conf.addObserver(moneyFormatObserver);
+
+        // Establecemos la configuración inicial (esto notificará de inmediato a los observadores)
         conf.setDefaultDateFormat(new SimpleDateFormat("yyyy/MM/dd"));
         conf.setMoneyFormat(new DecimalFormat("##.00"));
         System.out.println("Established configuration");
-        
-        //Se dan de alta lo observers
-        DateFormatObserver dateFormatObserver = new DateFormatObserver();
-        MoneyFormatObserver moneyFormatObserver = new MoneyFormatObserver();
-        conf.addObserver(dateFormatObserver);
-        conf.addObserver(moneyFormatObserver);
+
         System.out.println("");
-        
-        //Se cambia la fonfiguratión
+
+        // Realizamos cambios en la configuración para ver cómo se notifica automáticamente
         conf.setDefaultDateFormat(new SimpleDateFormat("dd/MM/yyyy"));
         conf.setMoneyFormat(new DecimalFormat("###,#00.00"));
+
         System.out.println("");
-        
-        //Se realiza otro cambio en la configuración.
+
+        // Otro cambio
         conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy/dd"));
         conf.setMoneyFormat(new DecimalFormat("###,#00"));
-        
+
+        // Removemos los observadores
         conf.removeObserver(dateFormatObserver);
         conf.removeObserver(moneyFormatObserver);
+
         System.out.println("");
-        
-        //Se realiza otro cambio en la configuración.
+
+        // Al hacer un nuevo cambio, ya no hay observadores que reciban notificaciones
         conf.setDefaultDateFormat(new SimpleDateFormat("MM/yyyy"));
         conf.setMoneyFormat(new DecimalFormat("###,##0.00"));
     }
